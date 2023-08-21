c432cf3cc10d90afd05880989b0b909db1ea0737
Tony Li <tony.li@automattic.com>
2023-08-22 11:27:13 +1200

    Add a pipeline to test the plugin itself

---
+++ .buildkite/dogfood-pipeline.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -euo pipefail
+
+pipeline_step() {
+    node_version=$1
+    pipeline_yml=$2
+    {
+        echo "  - label: ':node: Validate $node_version'"
+        echo "    command: .buildkite/verify_node.sh '$node_version'"
+        echo "    plugins:"
+        echo "      - automattic/nvm#${BUILDKITE_COMMIT}:"
+        echo "          version: '$node_version'"
+    } >> "$pipeline_yml"
+}
+
+pipeline_file=".buildkite/dogfood-pipeline.yml"
+echo "steps:" > "$pipeline_file"
+
+for version in "$@"; do
+    pipeline_step "$version" "$pipeline_file"
+done
+
+echo "--- :pipeline: Generated pipeline file"
+cat "$pipeline_file"
+
+if [[ "${BUILDKITE:-}" == "true" ]]; then
+    echo "--- :pipeline: Uploading pipeline"
+    buildkite-agent pipeline upload "$pipeline_file"
+fi
---
+++ .buildkite/verify_node.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -euo pipefail
+
+echo "--- :node: Current node version is $(node --version)"
+
+# shellcheck source=/dev/null
+source "$NVM_DIR/nvm.sh" --no-use
+
+echo "Checking if the active node version is '$1'"
+
+expected_version=$(nvm version-remote "$1")
+echo "'$1' is resolved to the node version $expected_version"
+
+current_version=$(nvm current)
+echo "Currently activated node version is $current_version"
+
+test "$current_version" == "$expected_version"
