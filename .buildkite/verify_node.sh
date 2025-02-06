34a5ec3ee3b942ba1c2f21f080e3aa4a54395750
Gio Lodi <gio@mokacoding.com>
2025-02-07 09:25:08 +1100

    Update left over "node" usages to "Node.js"

--- .buildkite/verify_node.sh
+++ .buildkite/verify_node.sh
@@ -2,19 +2,19 @@
 
 set -euo pipefail
 
-echo "--- :node: Current node version is $(node --version)"
+echo "--- :node: Current Node.js version is $(node --version)"
 
 echo "NVM_DIR is: $NVM_DIR"
 
 # shellcheck source=/dev/null
 source "$NVM_DIR/nvm.sh" --no-use
 
-echo "Checking if the active node version is '$1'"
+echo "Checking if the active Node.js version is '$1'"
 
 expected_version=$(nvm version-remote "$1")
-echo "'$1' is resolved to the node version $expected_version"
+echo "'$1' is resolved to the Node.js version $expected_version"
 
 current_version=$(nvm current)
-echo "Currently activated node version is $current_version"
+echo "Currently activated Node.js version is $current_version"
 
 test "$current_version" == "$expected_version"
